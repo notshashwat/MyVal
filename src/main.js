30853cbd66fa7491ade43225267b905039d7e212
mohit040 <mohit.chourasia2020@vitstudent.ac.in>
2021-04-11 09:06:57 +0530

    del

--- src/main.js
+++
@@ -1,7 +0,0 @@
-var Buffer = require('buffer').Buffer
-global.window.parseEmail = parse
-global.window.Buffer = Buffer
-var Require=require('dotenv').config();
-
-var nearestDate = require('nearest-date');
-global.window.nearestDate = nearestDate
